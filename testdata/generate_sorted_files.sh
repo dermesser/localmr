2969487e56a1df08be68e7fec13811c0163cc1e2
Lewin Bormann <lbo@spheniscida.de>
2016-02-05 21:38:25 +0000

    Fix testdata in VCS, add comment to generate* script

--- testdata/generate_sorted_files.sh
+++ testdata/generate_sorted_files.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script generates 11 files with random alphanumeric strings.
+# Those are used by certain tests to test and measure the performance of
+# sharded sort implementations.
+
 for I in {0..10}; do
     cat /dev/urandom | tr -dc "\n[:alnum:]" | egrep '[[:alnum:]]{15,}' | colrm 16 | head -n 1000000 | sort > sorted${I}.txt;
 done
