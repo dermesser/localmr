a5d1f96a623135c038e4434909d85ae2cd2969bb
Lewin Bormann <lbo@spheniscida.de>
2016-06-15 19:58:21 +0200

    Fix bug in SMI sorter

--- src/shard_merge.rs
+++ src/shard_merge.rs
@@ -69,7 +69,9 @@ impl<'a, T: Ord + Clone> ShardMergeIterator<'a, T> {
             right: Box::new(iter::empty()),
             left_peeked: None,
             right_peeked: None,
-            comparer: sort::default_generic_compare,
+            // map output is sorted in dictionary order (Aa, aa, Ba) instead of numeric (Aa, Ba,
+            // aa)
+            comparer: sort::dict_string_compare,
         }
     }
 
